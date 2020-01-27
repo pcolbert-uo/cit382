4f52b9af09fe3d3851cc17fcd89561861559d06d
pcolbert-uo <38925005+pcolbert-uo@users.noreply.github.com>
2020-01-27 15:27:48 -0800

    Create data.js

---
+++ 20w/assignments/A01/src/data.js
@@ -0,0 +1,46 @@
+/* 
+    Generate 10 random names: 
+    https://www.name-generator.org.uk/quick/
+
+    Generate 10 random integers:
+    https://www.random.org/integers/?num=10&min=1&max=10&col=1&base=10&format=html&rnd=new
+
+    Create quiz data 
+
+*/
+
+export const quizData = {
+  term: "20W",
+  quizzes: [
+    {
+      quizName: "Quiz 1",
+      scores: [
+        {
+          last: "Suarez",
+          first: "Ace",
+          score: 5
+        },
+        {
+          last: "Terrel",
+          first: "Keanu",
+          score: 7
+        }
+      ]
+    },
+    {
+      quizName: "Quiz 2",
+      scores: [
+        {
+          last: "Suarez",
+          first: "Ace",
+          score: 9
+        },
+        {
+          last: "Terrel",
+          first: "Keanu",
+          score: 7
+        }
+      ]
+    }
+  ]
+};
